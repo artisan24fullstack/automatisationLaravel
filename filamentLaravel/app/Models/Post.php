57251e0dc8dd047d72f480c55a792825c2f04919
artisan24fullstack <160485169+artisan24fullstack@users.noreply.github.com>
2024-07-05 10:41:03 +0200

    installation config

---
+++ filamentLaravel/app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'image',
+        'body',
+        'published_at',
+        'featured',
+    ];
+
+    public function author(){
+        return $this->belongsTo(User::class, "user_id");
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
